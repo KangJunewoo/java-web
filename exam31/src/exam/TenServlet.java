38173bf1f565ea314a32cbc7e1910d45d28f6bcd
KangJunewoo <brianjune.dev@gmail.com>
2021-04-06 15:48:16 +0900

    feat: 서블릿으로 1~10 출력하는 웹 소스코드 작성

--- exam31/src/exam/TenServlet.java
+++ exam31/src/exam/TenServlet.java
@@ -1,6 +1,8 @@
 package exam;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class TenServlet
  */
-@WebServlet("/ten")
+@WebServlet("/ttt")
 public class TenServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +28,13 @@ public class TenServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>1-10까지 출력!!<h1>");
+		for(int i=1;i<=10;i++) {
+			out.println(i+"<br>");
+		}
+		out.close();
 	}
 
 	/**
